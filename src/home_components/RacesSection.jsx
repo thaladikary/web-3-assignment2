e28bbdaed8304cbeecdc8d8773a9ff7fdc19b72d
tadik910 <tadik910@mtroyal.ca>
2024-03-28 23:16:20 -0600

    collapsble list test

--- src/home_components/RacesSection.jsx
+++ src/home_components/RacesSection.jsx
@@ -7,27 +7,36 @@ import {
   ListItemIcon,
   ListItemText,
   Collapse,
-  CircularProgress
-
+  CircularProgress,
+  ListItem,
+  Box,
+  Button,
 } from "@mui/material";
 
-import{
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
 
- ExpandLess,
- ExpandMore,
- 
-} from '@mui/icons-material';
-
-import AnalyticsIcon from '@mui/icons-material/Analytics';
+import AnalyticsIcon from "@mui/icons-material/Analytics";
 
 const RacesSection = () => {
-  const {races,setRaces,selectedSeason, setSelectedSeason } = useContext(AppContext);
-  const [open, setOpen] = useState(true);
+  const { races, setRaces, selectedSeason, setSelectedSeason } =
+    useContext(AppContext);
 
-  const handleClick = (race) => {
-    const sel = race.find(c => c.id == id);
-    console.log(race)
-    //setOpen(!open);
+  const [open, setOpen] = useState(true);
+  const [openItems, setOpenItems] = useState([]); // State to track which items are open
+
+  // const handleClick = (e) => {
+  //   // console.log(e);
+
+  //   // const sel = races.find((c) => c.raceId == id);
+  //   setOpen(!open);
+  // };
+
+  const handleClick = (index) => {
+    setOpenItems((prevState) => {
+      const newState = [...prevState]; // Create a copy of the openItems array
+      newState[index] = !newState[index]; // Toggle the state of the clicked item
+      return newState;
+    });
   };
 
   useEffect(() => {
@@ -41,49 +50,49 @@ const RacesSection = () => {
       } catch (err) {
         console.log(err);
       }
-    }
+    };
 
     fetchRacesData();
-  },[]);
-  console.log(races)
-
-
-
-
-
+  }, []);
 
   return (
-    <div className="h-screen w-screen bg-zinc-500">
-
+    <div className="h-screen w-screen bg-zinc-900">
       {!races ? (
-        // The reason we do this is because we are still waiting for the data to 
-        // loaded , so we put a loading animation while we wait, if we don't do 
-        // this then the races state will have undefined 
+        // The reason we do this is because we are still waiting for the data to
+        // loaded , so we put a loading animation while we wait, if we don't do
+        // this then the races state will have undefined
         <CircularProgress className="m-8" />
       ) : (
-        <List
-      
-        sx={{ width: '100%', maxWidth: 360 }}
-        component="nav"
-        aria-labelledby="nested-list-subheader"
-        subheader={
-          <ListSubheader component="div" id="nested-list-subheader" >
-            RACE LIST
-          </ListSubheader>
-        }
-        >
-        {races.map((race) => (
-           <ListItemButton key={race.id} onClick={ () => handleClick(race)}>
-
-          
-            <ListItemText primary={race.name}/>
-            {/* {open ? <ExpandLess /> : <ExpandMore />} */}
-
-          </ListItemButton>
-        ))}  
-      
-
-      </List>
+        <Box className="text-slate-100 overflow-auto w-1/4 h-full">
+          <List
+            sx={{ width: "100%", maxWidth: 360 }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+              <ListSubheader component="div" id="nested-list-subheader">
+                RACE LIST
+              </ListSubheader>
+            }
+          >
+            {races.map((race, index) => (
+              <div key={race.id}>
+                <ListItemButton onClick={() => handleClick(index)}>
+                  <ListItemText primary={race.name} />
+                </ListItemButton>
+                <Collapse in={openItems[index]} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    <Button onClick={() => handleButtonClick1(race)}>
+                      Button 1
+                    </Button>
+                    <Button onClick={() => handleButtonClick2(race)}>
+                      Button 2
+                    </Button>
+                  </List>
+                </Collapse>
+              </div>
+            ))}
+          </List>
+        </Box>
       )}
     </div>
   );
