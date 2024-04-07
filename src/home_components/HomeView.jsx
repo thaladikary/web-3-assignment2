5b0edc5c7777fd1aa80c1a7ff2a1df06b39b1f84
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2024-04-07 01:34:57 -0600

    99% done the driver modal

--- src/home_components/HomeView.jsx
+++ src/home_components/HomeView.jsx
@@ -74,7 +74,7 @@ const HomeView = ({ supabase }) => {
   };
 
   const fetchDriverData = async (driverRef) => {
-    
+
     const { data, error } = await supabase
       .from("results")
       .select(
@@ -83,7 +83,7 @@ const HomeView = ({ supabase }) => {
               fastestLap, rank, fastestLapTime, fastestLapSpeed, statusId, 
               drivers(driverRef, code, forename, surname,countrycode),
               races(name, round, year, date),
-              constructors(name, constructorRef, nationality)`
+              constructors(name, constructorRef, nationality,countrycode)`
       )
       .eq("raceId", selectedRace.raceId)
       .order("grid", { ascending: true });
