4d48da0d5d9e054883a369e0c82a868902bfb28b
tadik910 <tadik910@mtroyal.ca>
2024-04-08 13:18:51 -0600

    img

---
+++ src/home_components/SideDrawer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useContext, useEffect } from "react";
+import { AppContext } from "../Context";
+
+const SideDrawer = () => {
+  return <div></div>;
+};
+
+export default SideDrawer;
---
+++ src/views/SelectOption.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SelectOption = () => {
+  return (
+    <div>
+      <h1>FILTER</h1>
+      <h1>Favorites</h1>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default SelectOption;
